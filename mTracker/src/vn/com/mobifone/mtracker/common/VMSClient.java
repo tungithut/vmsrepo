1f189aeb34bab9632c66719213455e5146f99b36
Tung nguyen <tungithut@gmail.com>
2013-05-17 17:03:59 +0700

    clean the source.

--- mTracker/src/vn/com/mobifone/mtracker/common/VMSClient.java
+++ mTracker/src/vn/com/mobifone/mtracker/common/VMSClient.java
@@ -1,19 +1,4 @@
-/*
-*    This file is part of GPSLogger for Android.
-*
-*    GPSLogger for Android is free software: you can redistribute it and/or modify
-*    it under the terms of the GNU General Public License as published by
-*    the Free Software Foundation, either version 2 of the License, or
-*    (at your option) any later version.
-*
-*    GPSLogger for Android is distributed in the hope that it will be useful,
-*    but WITHOUT ANY WARRANTY; without even the implied warranty of
-*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*    GNU General Public License for more details.
-*
-*    You should have received a copy of the GNU General Public License
-*    along with GPSLogger for Android.  If not, see <http://www.gnu.org/licenses/>.
-*/
+
 
 package vn.com.mobifone.mtracker.common;
 
@@ -39,9 +24,9 @@ import vn.com.mobifone.mtracker.db.DatabaseHandler.Waypoints;
 
 
 /**
- * OpenGTS Client
+ * VMS Client to connect to VMS server
  *
- * @author Francisco Reynoso <franole @ gmail.com>
+ * @author Tungnt2
  */
 public class VMSClient
 {
